85502f03ae2a77a283b094495187ffaa4570db8c
MaesMaes <t.bitw.fl@yandex.ru>
2016-07-01 15:45:21 +0500

    Redesign, integration underscore
    Redesign search block and underscore template. Add image output.

--- index.php
+++ index.php
@@ -11,64 +11,88 @@ require_once( 'scripts/function.php' );
     <meta charset="UTF-8">
     <title>Кетл</title>
     <link rel="stylesheet" href="stylesheet/style_i.css">
-    <script src="scripts/jquery-2.2.0.min.js" language="javascript"></script>
-    <script src="scripts/script.js"></script>
-    <script src="scripts/scripts.js"></script>
+    <link rel="stylesheet" href="stylesheet/pads.min.css">
 </head>
 <body>
     <header class="head">
         <div class="container">
-            <div class="search">
-                 <p>Введите код колодки</p>
-                 <input placeholder="Пример: 1515KT" type="text" name="code" id="code">
-                 <div class="show_code">Показать</div>
+            <div class="block_search">
+                <div class="search">
+                     <p>Введите код колодки</p>
+                     <input placeholder="Пример: 1515KT" type="text" name="code" id="code">
+                     <div class="show_code">Показать</div>
+                </div>
+                <br/><br/><br/>
+                <h1> </h1>
+                <form name="car_producers" id="car_producers" >
+                    <div class="row">
+                        <label for="type">Класс:</label>
+                        <select id="type">
+                            <option value="легковой">Легковой</option>
+                            <option value="коммерческий">Коммерческий</option>
+                        </select>
+                    </div>
+                    <div class="row">
+                        <label for="producer">Марка:</label>
+                        <select id="producer">
+                            <option value="0">Выберите из списка</option>
+                            <?php
+                            $aProducers = getProducers();
+                            foreach ( $aProducers as $aProducer ) {
+                                print '<option value="' . $aProducer[ 'id' ] . '">' . $aProducer[ 'mark' ] . '</option>';
+                            }
+                            ?>
+                        </select>
+                    </div>
+                    <div class="row">
+                        <label for="model">Модель:</label>
+                        <select id="model" disabled >
+                            <option value="0">Выберите из списка</option>
+                        </select>
+                    </div>
+    		    </form>
+                <div class="show_pads">Показать</div>
+                <div class="clear"></div>
             </div>
-            <br/><br/><br/>
-            <h1> </h1>
-            <form name="car_producers" id="car_producers" >
-                <div class="row">
-                    <label for="type">Класс:</label>
-                    <select id="type">
-                        <option value="легковой">Легковой</option>
-                        <option value="коммерческий">Коммерческий</option>
-                    </select>
-                </div>
-                <div class="row">
-                    <label for="producer">Марка:</label>
-                    <select id="producer">
-                        <option value="0">Выберите из списка</option>
-                        <?php					
-                        $aProducers = getProducers();
-                        foreach ( $aProducers as $aProducer ) {
-                            print '<option value="' . $aProducer[ 'id' ] . '">' . $aProducer[ 'mark' ] . '</option>';
-                        }
-                        ?>
-                    </select>
-                </div>
-                <div class="row">
-                    <label for="model">Модель:</label>
-                    <select id="model" disabled >
-                        <option value="0">Выберите из списка</option>
-                    </select>
-                </div>
-		    </form>
-           
-            <div class="show_pads">Показать</div>
-            <table class="pads_tab">
-                <thead>
-                    <tr>
-                        <td>Производитель</td>
-                        <td>Номер по Кетл</td>
-                        <td>Оригинал</td>
-                        <td>Номер оригинала</td>
-                        <td>Описание</td>
-                    </tr>
-                </thead>
-                <tbody>
-                </tbody>
-            </table>
+            <div class="pads_tab"></div>
             <div class="no">По вашему запросу ничего не найдено</div>
         </div>
     </header>
+
+    <!-- BEGIN: Underscore Template Definition. -->
+    <script type="text/template" class="template">
+        <% _.each( rc.listItems, function( listItem ){ %>
+            <div class="pads_block">
+               <div class="pads_block-left">
+                   <div class="pads_block-left-img" style="background: url(images/<%- listItem.image %>) center center no-repeat, url(images/no_photo.jpg) center center no-repeat; background-size:cover;"></div>
+                   <div class="pads_block-left-desc">
+                       <div class="pads_block-left-desc-title">Номер Кетл:</div>
+                       <div class="pads_block-left-desc-value"><%- listItem.numKetl %></div>
+                       <div class="pads_block-left-desc-title">Оригинал:</div>
+                       <div class="pads_block-left-desc-value"><%- listItem.original %></div>
+                   </div>
+               </div>
+               <div class="pads_block-right">
+                   <div class="pads_block-right-tbl">
+                       <div class="pads_block-right-tbl-title">Номер оригинала</div>
+                       <div class="pads_block-right-tbl-val"><%- listItem.numOriginal %></div>
+                   </div>
+                   <div class="pads_block-right-tbl">
+                       <div class="pads_block-right-tbl-title">Номер TRW</div>
+                       <div class="pads_block-right-tbl-val"><%- listItem.numTRW %></div>
+                   </div>
+                   <div class="pads_block-right-desc">
+                       <%- listItem.desc %>
+                   </div>
+               </div>
+            </div>
+         <% }); %>
+    </script>
+    <!-- END: Underscore Template Definition. -->
+
+    <script src="scripts/jquery-2.2.0.min.js" language="javascript"></script>
+    <script src="node_modules/underscore/underscore-min.js"></script>
+    <script src="scripts/script.js"></script>
+    <script src="scripts/scripts.js"></script>
 </body>
 </html>
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,199 +1,238 @@
 (function($) {
 
-	// Включаем строгий режим ECMA-Script
-	"use strict";
+    // Включаем строгий режим ECMA-Script
+    "use strict";
 
-	/**
-	 * В скрипте мы будем выполнять AJAX-запросы к СУБД MySQL
-	 * Чтобы каждый раз не писать один и тот же код AJAX-запроса, создадим
-	 * свой метод request в объекте jQuery
-	 */
-	$.extend({
-		request: function( options ) {
+    /**
+     * В скрипте мы будем выполнять AJAX-запросы к СУБД MySQL
+     * Чтобы каждый раз не писать один и тот же код AJAX-запроса, создадим
+     * свой метод request в объекте jQuery
+     */
+    $.extend({
+        request: function(options) {
 
-			// В методе request будут различные опции (настройки)
-			// Это своего рода настройки по умолчанию, созданные
-			// в объекте options
-			options = $.extend({
+            // В методе request будут различные опции (настройки)
+            // Это своего рода настройки по умолчанию, созданные
+            // в объекте options
+            options = $.extend({
 
-				type: "POST",					// Метод передачи данных серверу
-				url: "scripts/requests.php",	// Путь к файлу со сценарием обращения к СУБД
-				data: null,						// Данные, которые мы будем передавать серверу
-				async: false,					// Асинхронность выполнения AJAX-запроса
-				dataType: "json",				// Тип данных, в котором они передаются
-				before: null,					// Код, выполняемый перед AJAX-запросом
-				error: function() {},			// Код, выполняемый в случае какой-либо ошибки при AJAX-запросе
-				complete: options.callback,		// Код, выполняемый после AJAX-запроса
-				success: function( result ) {	// Код, выполняемый после получения ответа от сервера
-					$.response.result = result;	// Помещаем ответ от сервера в отдельный объект
-				},
-				result: null,					// Результат работы
-				callback: null					// Функция обратного вызова
+                type: "POST", // Метод передачи данных серверу
+                url: "scripts/requests.php", // Путь к файлу со сценарием обращения к СУБД
+                data: null, // Данные, которые мы будем передавать серверу
+                async: false, // Асинхронность выполнения AJAX-запроса
+                dataType: "json", // Тип данных, в котором они передаются
+                before: null, // Код, выполняемый перед AJAX-запросом
+                error: function() {}, // Код, выполняемый в случае какой-либо ошибки при AJAX-запросе
+                complete: options.callback, // Код, выполняемый после AJAX-запроса
+                success: function(result) { // Код, выполняемый после получения ответа от сервера
+                    $.response.result = result; // Помещаем ответ от сервера в отдельный объект
+                },
+                result: null, // Результат работы
+                callback: null // Функция обратного вызова
 
-			}, options );
+            }, options);
 
-			options.before = function() {
-				alert( "ok before" );
-			};
+            options.before = function() {
+                alert("ok before");
+            };
 
-			// Тело AJAX-запроса
-			$.ajax({
+            // Тело AJAX-запроса
+            $.ajax({
 
-				type: options.type,
-				url: options.url,
-				data: options.data,
-				async: options.async,
-				dataType: options.dataType,
-				before: options.before,
-				error: options.error,
-				complete: options.complete,
-				success: options.success
+                type: options.type,
+                url: options.url,
+                data: options.data,
+                async: options.async,
+                dataType: options.dataType,
+                before: options.before,
+                error: options.error,
+                complete: options.complete,
+                success: options.success
 
-			});
+            });
 
-			return this;
+            return this;
 
-		},
-		// Объект, в котором хранится ответ от сервера, полученный через AJAX-запрос
-		response: {
-			result: {}
-		}
-	});
+        },
+        // Объект, в котором хранится ответ от сервера, полученный через AJAX-запрос
+        response: {
+            result: {}
+        }
+    });
 
-	jQuery(function() {
+    jQuery(function() {
 
         $('.show_pads').on('click', function() {
-            if ( $('#producer option:selected').val() != 0 && $('#model option:selected').val() != 0 ) {
+            if ($('#producer option:selected').val() != 0 && $('#model option:selected').val() != 0) {
                 jQuery.ajax({
                     type: 'POST',
                     url: 'scripts/requests.php',
-                    data: "request=getTable&mark=" + $('#producer option:selected').text() + "&model=" + $('#model option:selected').text()+ "&type=" + $('#type option:selected').val(),
+                    data: "request=getTable&mark=" + $('#producer option:selected').text() + "&model=" + $('#model option:selected').text() + "&type=" + $('#type option:selected').val(),
                     success: function(data) {
-                        var i = 0, pads = JSON.parse(data);
+                        var i = 0,
+                            pads = JSON.parse(data);
 
                         $('.pads_tab tbody tr').remove();
 
-                        if (pads.length>0) {
-                            for ( i; i < pads.length; i++ ) {
-
-                                $('.pads_tab tbody').append('<tr>' + '<td>' + pads[ i ].Mark + '</td>' + '<td>' + pads[ i ].Number_kitl + '</td>' + '<td>' + pads[ i ].Mark_origin + '</td>' + '<td>' + pads[ i ].Number_origin + '</td>' + '<td>' + pads[ i ].Name + '</td>' + '</tr>');
-
+                        _.templateSettings.variable = "rc";
+                        // Grab the HTML out of our template tag and pre-compile it.
+                        var template = _.template(
+                            $("script.template").html()
+                        );
+                        // Define our render data (to be put into the "rc" variable).
+                        var templateData = {
+                            listItems: []
+                        };
+                        $(".pads_tab").html('');
+                        if (pads.length > 0) {
+                            for (i; i < pads.length; i++) {
+                                templateData.listItems.push({
+                                    numKetl: pads[i].Number_kitl,
+                                    original: pads[i].Mark_origin,
+                                    numOriginal: pads[i].Number_origin,
+                                    numTRW: 'gbd2456',
+                                    desc: pads[i].Name,
+                                    image: '%20' + pads[i].Number_kitl.slice(0, -2) + 'KT.jpg'
+                                });
                             }
+                            $(".pads_tab").append(
+                                template(templateData)
+                            );
                             $('.no').fadeOut();
                             $('.pads_tab').fadeIn();
                         } else {
+                            $(".pads_tab").html('');
                             $('.pads_tab').fadeOut();
-                           $('.no').text('По вашему запросу ничего не найдено').fadeIn();
+                            $('.no').text('По вашему запросу ничего не найдено').fadeIn();
                         }
-
-
-
-
                     },
-                    error:  function(xhr, str){
-                       console.info('Возникла ошибка');
+                    error: function(xhr, str) {
+                        console.info('Возникла ошибка');
                     }
                 });
             } else {
+				$(".pads_tab").html('');
                 $('.no').text('Выберите марку и модель автомобиля').fadeIn();
             }
         });
 
         $('.show_code').on('click', function() {
-            if ( $('#code').val() ) {
+            if ($('#code').val()) {
                 jQuery.ajax({
                     type: 'POST',
                     url: 'scripts/requests.php',
                     data: "request=getCodeTable&code=" + $('#code').val(),
                     success: function(data) {
-                        var i = 0, pads = JSON.parse(data);
+                        var i = 0,
+                            pads = JSON.parse(data);
 
                         $('.pads_tab tbody tr').remove();
 
-                        if (pads.length>0) {
-                            for ( i; i < pads.length; i++ ) {
-
-                                $('.pads_tab tbody').append('<tr>' + '<td>' + pads[ i ].Mark + '</td>' + '<td>' + pads[ i ].Number_kitl + '</td>' + '<td>' + pads[ i ].Mark_origin + '</td>' + '<td>' + pads[ i ].Number_origin + '</td>' + '<td>' + pads[ i ].Name + '</td>' + '</tr>');
-
+                        _.templateSettings.variable = "rc";
+                        // Grab the HTML out of our template tag and pre-compile it.
+                        var template = _.template(
+                            $("script.template").html()
+                        );
+                        // Define our render data (to be put into the "rc" variable).
+                        var templateData = {
+                            listItems: []
+                        };
+                        $(".pads_tab").html('');
+                        if (pads.length > 0) {
+                            for (i; i < pads.length; i++) {
+                                templateData.listItems.push({
+                                    numKetl: pads[i].Number_kitl,
+                                    original: pads[i].Mark_origin,
+                                    numOriginal: pads[i].Number_origin,
+                                    numTRW: 'gbd2456',
+                                    desc: pads[i].Name,
+                                    image: '%20' + pads[i].Number_kitl.slice(0, -2) + 'KT.jpg'
+                                });
                             }
+                            $(".pads_tab").append(
+                                template(templateData)
+                            );
                             $('.no').fadeOut();
                             $('.pads_tab').fadeIn();
                         } else {
+                            $(".pads_tab").html('');
                             $('.pads_tab').fadeOut();
                             $('.no').text('По вашему запросу ничего не найдено').fadeIn();
                         }
 
 
                     },
-                    error:  function(xhr, str){
-                       console.info('Возникла ошибка');
+                    error: function(xhr, str) {
+                        console.info('Возникла ошибка');
                     }
                 });
             } else {
+				$(".pads_tab").html('');
                 $('.no').text('Введите код Кетл или иностранного производителя').fadeIn();
             }
         });
 
-		/**
-		 *  При выборе производителя нужно сделать многое
-		 *  Сначала из списка моделей должны быть удалены все имеющиеся модели автомобилей
-		 *  Затем поле выбора модели автомобиля должно стать неактивным
-		 */
+        /**
+         *  При выборе производителя нужно сделать многое
+         *  Сначала из списка моделей должны быть удалены все имеющиеся модели автомобилей
+         *  Затем поле выбора модели автомобиля должно стать неактивным
+         */
 
-		// Обработчик события выбора производителя
-		$( '#producer' ).change(function() {
+        // Обработчик события выбора производителя
+        $('#producer').change(function() {
 
-			var producer_id = $( this ).val();	// Идентификатор выбранного производителя
-
-
-
-			// Отключаем поле, установив значения свойства disabled
-			$( '#model' ).prop( 'disabled', true )
-
-			// Находим и удаляем все возможные модели автомобилей из раскрывающегося списка
-			.find( 'option:not( :first )' ).remove();
+            var producer_id = $(this).val(); // Идентификатор выбранного производителя
 
 
 
-			// Если был выбран конкретный производитель
-			if ( producer_id != 0 ) {
+            // Отключаем поле, установив значения свойства disabled
+            $('#model').prop('disabled', true)
 
-				// Создаем AJAX-запрос, который вернет нам перечень моделей для выбранной марки
-				//$.request({
+            // Находим и удаляем все возможные модели автомобилей из раскрывающегося списка
+            .find('option:not( :first )').remove();
 
-					//data: "request=getModels&producer_id=" + producer_id,
 
-				//});
+
+            // Если был выбран конкретный производитель
+            if (producer_id != 0) {
+
+                // Создаем AJAX-запрос, который вернет нам перечень моделей для выбранной марки
+                //$.request({
+
+                //data: "request=getModels&producer_id=" + producer_id,
+
+                //});
                 jQuery.ajax({
                     type: 'POST',
                     url: 'scripts/requests.php',
                     data: "request=getModels&producer_id=" + producer_id,
                     success: function(data) {
                         // Успешный AJAX-запрос должен закончиться вставкой полученного перечня моделей
-				        // в раскрывающийся список select#model
-				        // Результат AJAX-запроса мы сохраняли в отдельном объекте
-                        var i = 0, models = JSON.parse(data);
+                        // в раскрывающийся список select#model
+                        // Результат AJAX-запроса мы сохраняли в отдельном объекте
+                        var i = 0,
+                            models = JSON.parse(data);
 
-                        for ( i; i < models.length; i++ ) {
+                        for (i; i < models.length; i++) {
 
-                            $( '#model' ).append( '<option value="' + models[ i ].mark_id + '">' + models[ i ].model + '</option>' );
+                            $('#model').append('<option value="' + models[i].mark_id + '">' + models[i].model + '</option>');
 
                         }
-				        // Включаем поле со списком моделей
-				        $( '#model' ).prop( 'disabled', false );
+                        // Включаем поле со списком моделей
+                        $('#model').prop('disabled', false);
 
                     },
-                    error:  function(xhr, str){
-                       console.info('Возникла ошибка');
+                    error: function(xhr, str) {
+                        console.info('Возникла ошибка');
                     }
                 });
 
 
 
-			}
+            }
 
-		}); // Обработчик события выбора производителя
+        }); // Обработчик события выбора производителя
 
-	});
+    });
 
 })(jQuery); // Используем немедленно вызываемую анонимную функцию
